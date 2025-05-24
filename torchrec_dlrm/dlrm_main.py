92502227418199952ec5dc051704233d165cc8fc
Anhad Singh <114313991+notanhad@users.noreply.github.com>
2025-05-25 00:17:07 +0100

    Update dlrm_main.py

--- torchrec_dlrm/dlrm_main.py
+++ torchrec_dlrm/dlrm_main.py
@@ -563,7 +563,7 @@ def main(argv: List[str]) -> None:
     ), "--multi_hot_distribution_type is used to convert 1-hot to multi-hot. It's inapplicable with --synthetic_multi_hot_criteo_path."
 
     # rank = int(os.environ["LOCAL_RANK"])
-    rank = 0
+    rank = torch.cuda.device_count()
     if torch.cuda.is_available():
         device: torch.device = torch.device(f"cuda:{rank}")
         backend = "nccl"
@@ -577,7 +577,7 @@ def main(argv: List[str]) -> None:
             "PARAMS: (lr, batch_size, warmup_steps, decay_start, decay_steps): "
             f"{(args.learning_rate, args.batch_size, args.lr_warmup_steps, args.lr_decay_start, args.lr_decay_steps)}"
         )
-    dist.init_process_group(backend=backend)
+    dist.init_process_group(backend=backend, init_method='env://', rank = torch.cuda.device_count(), world_size = 1)
 
     if args.num_embeddings_per_feature is not None:
         args.num_embeddings = None
