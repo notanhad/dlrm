d7dba66e68ab3357b0d62be179f4234b42180cca
Anhad Singh <114313991+notanhad@users.noreply.github.com>
2025-05-25 13:23:20 +0100

    Update dlrm_main.py

--- torchrec_dlrm/dlrm_main.py
+++ torchrec_dlrm/dlrm_main.py
@@ -707,15 +707,15 @@ def main(argv: List[str]) -> None:
             compute_device=device.type,
         ),
         batch_size=args.batch_size,
+        # If experience OOM, increase the percentage. see
+        # https://pytorch.org/torchrec/torchrec.distributed.planner.html#torchrec.distributed.planner.storage_reservations.HeuristicalStorageReservation
+        storage_reservation=HeuristicalStorageReservation(percentage=0.05),
         constraints = {
             "large_table": ParameterConstraints(
                 sharding_types=["table_wise"],
                 compute_kernels=[EmbeddingComputeKernel.BATCHED_FUSED_UVM.value],
             )
         }
-        # If experience OOM, increase the percentage. see
-        # https://pytorch.org/torchrec/torchrec.distributed.planner.html#torchrec.distributed.planner.storage_reservations.HeuristicalStorageReservation
-        storage_reservation=HeuristicalStorageReservation(percentage=0.05),
     )
     plan = planner.collective_plan(
         train_model, get_default_sharders(), dist.GroupMember.WORLD
